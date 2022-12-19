efec1a5c1f5b55d358a4955fcd48807197151d38
NAVNEET CHAUHAN <cn5508119@gmail.com>
2022-12-19 23:43:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(require('./router/register'));
 console.log("Server started");
 app.get('/',(req,res)=>{
         res.send("Hello");
